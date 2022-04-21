ef67fddf03704f85e3bf5cc3a717662f6a40b641
josemartinmolina <jose.molina@tec.mx>
2022-04-21 13:20:20 -0500

    Initial commit

---
+++ src/main/java/com/itesm/azul/services/PersonaService.java
@@ -0,0 +1,60 @@
+package com.itesm.azul.services;
+
+
+import com.itesm.azul.models.Persona;
+import com.itesm.azul.dto.PersonDTO;
+import com.itesm.azul.repositories.PersonaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PersonaService {
+
+    @Autowired
+    PersonaRepository personaRepository;
+    //CREATE
+    public void createPersona(final PersonDTO persona){
+        Persona p = new Persona();
+        p.setEdad(persona.getEdad());
+        p.setPersonaID(persona.getPersonaID());
+        p.setNombre(persona.getNombre());
+        personaRepository.save(p);
+    }
+    //READ
+    public Iterable<Persona> lista(){
+        return personaRepository.findAll();
+    }
+    public Persona getOne(String personaID){
+        return personaRepository.findById(personaID).get();
+    }
+    public boolean existsId(String personaID){
+        return personaRepository.existsById(personaID);
+    }
+    //UPDATE
+    public Persona update(PersonDTO dto){
+        Persona persona = personaRepository.findById(dto.getPersonaID()).get();
+        persona.setPersonaID(dto.getPersonaID());
+        persona.setNombre(dto.getNombre());
+        persona.setEdad(dto.getEdad());
+        return personaRepository.save(persona);
+    }
+    //DELETE
+    public void delete(String personID){
+        personaRepository.deleteById(personID);
+    }
+
+
+
+/*
+    public Persona insertar(Persona persona) throws Exception{
+        if(persona.getEdad()<18){
+            throw new Exception("No puedes ser menor de edad");
+        }
+        //personaRepository.insert();
+        return persona;
+    }
+*/
+
+}
